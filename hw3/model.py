d7159853202b7ed46773cbcab685a0675cc07d12
j1a0m0e4s <b04901069@ntu.edu.tw>
2019-03-26 18:22:11 +0000

    t

--- hw3/model.py
+++ hw3/model.py
@@ -6,22 +6,22 @@ class Model_basic(nn.Module): # Overfitting - train acc:0.8 valid acc:0.5
     def __init__(self, class_num= 6):
         super().__init__()
         
-        # self.conv_block = nn.Sequential(
-        #     nn.Conv2d(1, 16, kernel_size= 3, stride= 1, padding= 1),
-        #     nn.ReLU(inplace= True),
-        #     nn.Conv2d(16, 64, kernel_size= 4, stride= 2, padding= 1),
-        #     nn.ReLU(inplace= True),
-        #     nn.Conv2d(64, 128, kernel_size= 4, stride= 2, padding= 1),
-        #     nn.ReLU(inplace= True),
-        #     nn.Conv2d(128, 128, kernel_size= 4, stride= 2, padding= 1),
-        #     nn.ReLU(inplace= True)
-        # )
         self.conv_block = nn.Sequential(
-            ResidualBlock(1, 16, kernel_size= 4, stride= 2, padding= 1),
-            ResidualBlock(16, 64, kernel_size= 4, stride= 2, padding= 1),
+            nn.Conv2d(1, 16, kernel_size= 3, stride= 1, padding= 1),
+            nn.ReLU(inplace= True),
+            nn.Conv2d(16, 64, kernel_size= 4, stride= 2, padding= 1),
+            nn.ReLU(inplace= True),
+            nn.Conv2d(64, 128, kernel_size= 4, stride= 2, padding= 1),
+            nn.ReLU(inplace= True),
+            nn.Conv2d(128, 128, kernel_size= 4, stride= 2, padding= 1),
+            nn.ReLU(inplace= True)
         )
+        # self.conv_block = nn.Sequential(
+        #     ResidualBlock(1, 16, kernel_size= 4, stride= 2, padding= 1),
+        #     ResidualBlock(16, 64, kernel_size= 4, stride= 2, padding= 1),
+        # )
 
-        self.fc1 = nn.Linear(64*12*12, 128)
+        self.fc1 = nn.Linear(128*6*6, 128)
         self.fc2 = nn.Linear(128, class_num)
 
     def forward(self, inputs):
@@ -39,14 +39,14 @@ class Model_1(nn.Module):
             ResidualBlock(16, 64, kernel_size= 4, stride= 2, padding= 1),
         )
 
-        self.fc1 = nn.Linear(64 * 12 * 12, 128)
-        self.fc2 = nn.Linear(128, class_num)
+        self.fc1 = nn.Linear(64 * 12 * 12, class_num)
+        # self.fc2 = nn.Linear(128, class_num)
 
     def forward(self, inputs):
         x = self.conv_block(inputs)
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        # x = self.fc2(x)
         return x
 
 class ResidualBlock(nn.Module):
@@ -88,7 +88,7 @@ def parameter_number(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 def test():
-    model = Model_basic()
+    model = Model_1()
     imgs = torch.zeros(4, 1, 48, 48)
     out = model(imgs)
     
