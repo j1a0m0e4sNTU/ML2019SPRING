eabfca8fd595c9a23ec2eead183e73f64be7290a
James Lin <b04901069@ntu.edu.tw>
2019-03-28 00:58:30 +0800

    add experiment C

--- hw3/experiment.sh
+++ hw3/experiment.sh
@@ -1,3 +1,3 @@
 # python3 main.py train -epoch 50 -save ../../weights/01.pkl
-python3 main.py train -epoch 50 -save ../../weights/vgg_b.pkl
-# python3 main.py train -epoch 50 -save ../../weights/vgg_c.pkl
+# python3 main.py train -epoch 50 -save ../../weights/vgg_b.pkl
+python3 main.py train -epoch 50 -save ../../weights/vgg_c.pkl
