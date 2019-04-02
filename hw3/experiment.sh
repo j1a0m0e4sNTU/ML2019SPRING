091d825ebf1c3c68cd133fb4d23a6da46824e0f4
James Lin <b04901069@ntu.edu.tw>
2019-04-02 16:01:55 +0800

    add transformation

--- hw3/experiment.sh
+++ hw3/experiment.sh
@@ -2,4 +2,5 @@
 # python3 main.py train -epoch 50 -save ../../weights/vgg_b.pkl
 # python3 main.py train -epoch 50 -save ../../weights/vgg_c.pkl
 # python3 main.py train -epoch 50 -save ../../weights/vgg_c_2.pkl
-python3 main.py train -epoch 50 -save ../../weights/vgg_c_3.pkl
+# python3 main.py train -epoch 50 -save ../../weights/vgg_c_3.pkl
+python3 main.py train -epoch 50 -save ../../weights/vgg_c_4.pkl
