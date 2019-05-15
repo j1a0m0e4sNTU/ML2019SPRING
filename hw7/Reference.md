55aa28bf8b363489bea131c6644453e046336667
James Lin <b04901069@ntu.edu.tw>
2019-05-16 02:49:15 +0800

    eigen face

--- hw7/Reference.md
+++ hw7/Reference.md
@@ -1,3 +1,4 @@
 # Reference
 
 * [Sklearn - Clustering](https://scikit-learn.org/stable/modules/clustering.html)
+* [維基 - 特徵臉](https://zh.wikipedia.org/wiki/%E7%89%B9%E5%BE%81%E8%84%B8)
--- hw7/pca.py
+++ hw7/pca.py
@@ -16,12 +16,36 @@ def get_images():
         images[i] = np.array(io.imread(name))
     return images
 
-def main():
-    pass
+def eigen(vectors):
+    conv_matrix = (vectors) @ vectors.T
+    eigen_values, eigen_vectors = np.linalg.eig(conv_matrix)
+    return eigen_values, eigen_vectors
+    
+def show_image(vector):
+    image = vector.reshape((600, 600, 3))
+    image -= np.min(image)
+    image /= np.max(image)
+    image = (image * 255).astype(np.uint8)
+    io.imshow(image)
+    io.show()
 
+def main():
+    images = get_images()
+    vectors = images.reshape((images.shape[0], -1))# (415, 1080000)
+    mean = np.mean(vectors, 0)
+    for i in range(len(mean)):
+        vectors[:, i] -= mean[i] 
+    eigen_values, eigen_vectors = eigen(vectors)
+
+    for i in range(10, 15):
+        print('Eigen Value: {}'.format(eigen_values[i]))
+        eig_v = eigen_vectors[:, i]
+        eig_face = (vectors.T) @ eig_v 
+        show_image(eig_face)
+    
 def test():
     images = get_images()
     
     
 if __name__ == '__main__':
-    test()
+    main()
