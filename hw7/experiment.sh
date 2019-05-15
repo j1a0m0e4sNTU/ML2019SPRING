2cc474475cc9a68df60657116821038f1d5bd523
j1a0m0e4s <b04901069@ntu.edu.tw>
2019-05-15 07:18:48 +0000

    add

--- hw7/experiment.sh
+++ hw7/experiment.sh
@@ -5,5 +5,5 @@
 # python3 main.py train -E D -D D -id 0514_D_D -save ../../weights/0514_D_D.pkl
 # python3 main.py cluster -E D -D D -load ../../weights/0514_D_D.pkl -csv test.csv
 # python3 main.py cluster -cluster_num 4 -csv test4.csv
-python3 main.py train -id simple_all -save ../../weights/simple_all.pkl
-python3 main.py cluster -load ../../weights/ismple_all.pkl 
+# python3 main.py train -id simple_all -save ../../weights/simple_all.pkl
+python3 main.py cluster -load ../../weights/simple_all.pkl 
