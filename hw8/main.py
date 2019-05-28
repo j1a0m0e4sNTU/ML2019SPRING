364165fab7fa2b2b501cedb602e4cf2b16b11de3
James Lin <b04901069@ntu.edu.tw>
2019-05-29 00:45:39 +0800

    add mobile nets

--- hw8/main.py
+++ hw8/main.py
@@ -4,15 +4,16 @@ from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from dataset import *
 from manager import Manager
-
-model = None
+from model import MobileNet
 
 parser = argparse.ArgumentParser()
 parser.add_argument('mode', help= 'Task: train/predict', choices=['train', 'predict'])
+parser.add_argument('-conv', help= 'Symbol for conv layer configuration', default= 'base')
+parser.add_argument('-fc', help= 'Symbol for fc layer configuration', default= 'base')
 parser.add_argument('-dataset', help= 'Path to dataset', default= '../../data_hw8/train.csv')
 parser.add_argument('-bs', help= 'batch size', type= int, default= 64)
 parser.add_argument('-lr', help= 'learnig rate', type= float, default= 1e-4)
-parser.add_argument('-epoch', help= 'Epoch number', type= int, default= 10)
+parser.add_argument('-epoch', help= 'Epoch number', type= int, default= 30)
 parser.add_argument('-save', help= 'Path to save model')
 parser.add_argument('-load', help= 'Path to load model')
 parser.add_argument('-csv', help= 'Path to prediction file')
@@ -21,6 +22,8 @@ parser.add_argument('-tencrop', help= 'Apply TenCrop to testdata', type= int, de
 args = parser.parse_args()
 
 def main():
+    model = MobileNet(args.conv, args.fc)
+
     if args.mode == 'train':
         print('Training ...')
         train_transform = transforms.Compose([
--- hw8/model.py
+++ hw8/model.py
@@ -1,6 +1,46 @@
 import torch
 import torch.nn as nn
 
+# simple baseline : about 162435 parameters
+# strong baseline : about 56310  parameters
+
+conv_config = {
+    'base': [16, 64, 'D', 128, 'D', 256, 'D', 512, 'D'],
+}
+
+fc_config = {
+    'base': [512*2*2, 128, 7],
+}
+
+def conv_layers(config):
+    layers = []
+    in_planes = 1
+    for x in config:
+        if x == 'D':
+            layers.append(nn.MaxPool2d(kernel_size= 2, stride= 2))
+        else:
+            layers += [
+                MobileConv2d(in_planes, x, 3, 1, 1), 
+                nn.ReLU(inplace= True)
+            ]
+            in_planes = x
+
+    layers = nn.Sequential(* layers)
+    return layers
+
+def fc_layers(config):
+    layers = []
+    for i in range(1, len(config)):
+        print(config[i - 1], config[i])
+        layers += [
+            nn.Linear(config[i - 1], config[i]), 
+            nn.ReLU(inplace= True),
+            nn.Dropout()
+        ]
+    layers = layers[:-2]
+    layers = nn.Sequential(* layers)
+    return layers
+
 class MobileConv2d(nn.Module):
     def __init__(self, in_planes, out_planes, kernel_size= 3, stride= 1, padding= 1):
         super().__init__()
@@ -20,14 +60,27 @@ class MobileConv2d(nn.Module):
 def parameter_number(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
+class MobileNet(nn.Module):
+    def __init__(self, conv_symbol, fc_symbol):
+        super().__init__()
+        self.conv_layers = conv_layers(conv_config[conv_symbol])
+        self.fc_layers = fc_layers(fc_config[fc_symbol])
+
+    def forward(self, inputs):
+        out = self.conv_layers(inputs)
+        out = out.view(out.size(0), -1)
+        out = self.fc_layers(out)
+        return out
+
 def test():
     batch_size = 8
-    images = torch.zeros(batch_size, 3, 48, 48)
-    model = MobileConv2d(3, 16, 4, 2, 1)
+    images = torch.zeros(batch_size, 1, 44, 44)
+    model = MobileNet('base', 'base')
     output = model(images)
 
     print(model)
     print('Model parameter number: {}'.format(parameter_number(model)))
+    print('Input size:  {}'.format(images.size()))
     print('Output size: {}'.format(output.size()))
 
 def test2():
@@ -37,6 +90,7 @@ def test2():
     conv = nn.Conv2d(in_c, out_c, kernel_size, 1, 0)
     print('Parameter num of mobile block: {}'.format(parameter_number(mobile)))
     print('Parameter num of conv: {}'.format(parameter_number(conv)))
+
 
 if __name__ == '__main__':
     test()
