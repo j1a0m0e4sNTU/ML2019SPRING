7d9848e3fdc7de99df030ae82479bf86be083f4b
James Lin <b04901069@ntu.edu.tw>
2019-05-29 02:09:54 +0800

    add exps

--- hw8/experiment.sh
+++ hw8/experiment.sh
@@ -1,2 +1,4 @@
 # python3 main.py train -save ../../weights/0529_test.pkl -record records/0529_test.txt
-python3 main.py train -conv A -fc A -save ../../weights/0529_A_A.pkl -record records/0529_A_A.txt
+# python3 main.py train -conv A -fc A -save ../../weights/0529_A_A.pkl -record records/0529_A_A.txt
+python3 main.py train -conv B -fc A -save ../../weights/0529_B_A.pkl -record records/0529_B_A.txt
+python3 main.py train -conv B -fc B -save ../../weights/0529_B_B.pkl -record records/0529_B_B.txt
